e259042513be2dec5b3ffdd046da301ed6e6712f
zhanggang <zhang834826337@qq.com>
2017-09-14 15:26:07 +0800

    百度地图定位

---
+++ app/src/main/java/com/example/zhanggang/ditu/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.zhanggang.ditu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    public LocationClient mLocationClient = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.textview);
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(new MyLocationListener());
+
+        initLocation();
+
+        mLocationClient.start(); //开启
+
+    }
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+
+        option.setCoorType("bd09ll");   //经纬度
+        //可选，默认gcj02，设置返回的定位结果坐标系
+//
+//        int span=1000;
+//        option.setScanSpan(span);
+//        //可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+
+        option.setIsNeedAddress(true);
+        //可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);
+        //可选，默认false,设置是否使用gps
+
+        option.setLocationNotify(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+//
+//        option.SetIgnoreCacheException(false);
+//        //可选，默认false，设置是否收集CRASH信息，默认收集
+//
+//        option.setEnableSimulateGps(false);
+//        //可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            //获取定位结果
+            location.getTime();    //获取定位时间
+            location.getLocationID();    //获取定位唯一ID，v7.2版本新增，用于排查定位问题
+            location.getLocType();    //获取定位类型
+            location.getLatitude();    //获取纬度信息
+            location.getLongitude();    //获取经度信息
+            location.getRadius();    //获取定位精准度
+            String addrStr = location.getAddrStr();//获取地址信息
+            location.getCountry();    //获取国家信息
+            location.getCountryCode();    //获取国家码
+            location.getCity();    //获取城市信息
+            location.getCityCode();    //获取城市码
+            location.getDistrict();    //获取区县信息
+            location.getStreet();    //获取街道信息
+            location.getStreetNumber();    //获取街道码
+            location.getLocationDescribe();    //获取当前位置描述信息
+            location.getPoiList();    //获取当前位置周边POI信息
+
+            location.getBuildingID();    //室内精准定位下，获取楼宇ID
+            location.getBuildingName();    //室内精准定位下，获取楼宇名称
+            location.getFloor();    //室内精准定位下，获取当前位置所处的楼层信息
+            textView.setText(addrStr);
+        }
+
+    }
+
+}
+
